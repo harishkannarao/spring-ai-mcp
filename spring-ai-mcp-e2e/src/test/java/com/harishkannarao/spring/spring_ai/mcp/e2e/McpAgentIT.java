94c32f7f4d3daf3dddc8c689ea0ace85da1f3bc2
harishkannarao <harishkannarao@users.noreply.github.com>
2025-06-25 17:24:27 +0100

    Verifying tool calls

--- spring-ai-mcp-e2e/src/test/java/com/harishkannarao/spring/spring_ai/mcp/e2e/McpAgentIT.java
+++ spring-ai-mcp-e2e/src/test/java/com/harishkannarao/spring/spring_ai/mcp/e2e/McpAgentIT.java
@@ -2,8 +2,9 @@ package com.harishkannarao.spring.spring_ai.mcp.e2e;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -45,4 +46,86 @@ public class McpAgentIT extends AbstractBaseIT {
 				text -> assertThat(text).containsIgnoringCase("Book2")
 			);
 	}
+
+	@Test
+	public void get_available_tickets_using_lookup() {
+		String input = """
+			How many tickets are available for Avatar Movie?""";
+
+		Response response = mcpAgentRestClient()
+			.accept(ContentType.TEXT)
+			.param("q", input)
+			.get("/chat-with-tools");
+
+		assertThat(response.statusCode()).isEqualTo(200);
+
+		String respText = response.body().asString();
+		assertThat(respText)
+			.containsIgnoringCase("Avatar")
+			.containsIgnoringCase("available");
+	}
+
+	@Test
+	public void book_movie_tickets() {
+		String input = """
+			Book 3 tickets for Avatar Movie""";
+
+		Response response = mcpAgentRestClient()
+			.accept(ContentType.TEXT)
+			.param("q", input)
+			.get("/chat-with-tools");
+
+		assertThat(response.statusCode()).isEqualTo(200);
+
+		String respText = response.body().asString();
+		assertThat(respText)
+			.containsIgnoringCase("Avatar")
+			.containsIgnoringCase("A1")
+			.containsIgnoringCase("A2")
+			.containsIgnoringCase("A3");
+	}
+
+	@ParameterizedTest
+	@CsvSource({
+		"London,20",
+		"Chennai,37"
+	})
+	public void get_weather_info_for_different_cities(String city, Integer expectedWeather) {
+		String input = """
+			What is the current weather in %s""".formatted(city);
+
+		Response response = mcpAgentRestClient()
+			.accept(ContentType.TEXT)
+			.param("q", input)
+			.get("/chat-with-tools");
+
+		assertThat(response.statusCode()).isEqualTo(200);
+
+		String respText = response.body().asString();
+		assertThat(respText)
+			.containsIgnoringCase(city)
+			.containsIgnoringCase(expectedWeather.toString());
+	}
+
+	@Test
+	public void book_suggestion_by_genre() {
+		String input = """
+			Suggest 4 good books in Mystery Genre""";
+
+		Response response = mcpAgentRestClient()
+			.accept(ContentType.TEXT)
+			.param("q", input)
+			.get("/chat-with-tools");
+
+		assertThat(response.statusCode()).isEqualTo(200);
+
+		String respText = response.body().asString();
+		assertThat(respText)
+			.containsIgnoringCase("Mystery")
+			.containsIgnoringWhitespaces("Book 5")
+			.containsIgnoringWhitespaces("Book 6")
+			.containsIgnoringWhitespaces("Book 7")
+			.containsIgnoringWhitespaces("Book 8");
+	}
+
 }
